dd4bae9fdaabf8c36db79484505c1392578edc94
gaudus <ichhabads@gmail.com>
2017-08-04 20:49:56 +0200

    tweek

--- src/main/java/mrriegel/transprot/ItemLinker.java
+++ src/main/java/mrriegel/transprot/ItemLinker.java
@@ -33,7 +33,7 @@ public class ItemLinker extends CommonItem {
 			if (world.getTileEntity(pos) instanceof TileDispatcher) {
 				NBTStackHelper.set(stack, "pos", pos);
 				NBTStackHelper.set(stack, "dim", world.provider.getDimension());
-				player.sendMessage(new TextComponentString("Bound to Dispatcher."));
+				player.sendStatusMessage(new TextComponentString("Bound to Dispatcher."),true);
 				return EnumActionResult.SUCCESS;
 			} else if (InvHelper.hasItemHandler(world, pos, facing) && NBTHelper.hasTag(stack.getTagCompound(), "pos")) {
 				BlockPos tPos = NBTStackHelper.get(stack, "pos", BlockPos.class);
@@ -43,13 +43,13 @@ public class ItemLinker extends CommonItem {
 					if (pos.getDistance(tPos.getX(), tPos.getY(), tPos.getZ()) < ConfigHandler.range) {
 						boolean done = tile.getTargets().add(pair);
 						if (done) {
-							player.sendMessage(new TextComponentString("Added " + world.getBlockState(pos).getBlock().getLocalizedName() + "."));
+							player.sendStatusMessage(new TextComponentString("Added " + world.getBlockState(pos).getBlock().getLocalizedName() + "."),true);
 							tile.sync();
 						} else {
-							player.sendMessage(new TextComponentString("Inventory is already connected."));
+							player.sendStatusMessage(new TextComponentString("Inventory is already connected."),true);
 						}
 					} else
-						player.sendMessage(new TextComponentString("Too far away."));
+						player.sendStatusMessage(new TextComponentString("Too far away."),true);
 					return EnumActionResult.SUCCESS;
 				}
 			}
